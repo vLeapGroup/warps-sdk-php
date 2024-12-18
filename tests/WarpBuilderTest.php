bfd9852e10303832d1b676b84d36f4342a5ca94e
Micha Vie <micha@vleap.io>
2024-12-18 23:32:35 +0100

    update test

--- tests/WarpBuilderTest.php
+++ tests/WarpBuilderTest.php
@@ -2,11 +2,13 @@
 
 use Vleap\Warp;
 use Vleap\WarpBuilder;
+use Vleap\Actions\ContractAction;
 
 it('createFromRaw', function () {
     $warpRaw = json_decode(file_get_contents(__DIR__.'/examples/create-token.json'), true);
 
-    $warp = WarpBuilder::createFromRaw($warpRaw);
+    $actual = WarpBuilder::createFromRaw($warpRaw);
 
-    expect($warp)->toBeInstanceOf(Warp::class);
+    expect($actual)->toBeInstanceOf(Warp::class);
+    expect($actual->actions->first())->toBeInstanceOf(ContractAction::class);
 });
